e387464d9acfcbd428ec3ee32daa96dfa4c55164
Alex Shapiro <acjshapiro@gmail.com>
2018-03-15 10:40:00 -0600

    register/login form

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,17 @@ import Register from './components/Register';
 import { Switch, Route } from 'react-router-dom';
 
 
-
+const App = () => (
+  <div>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route component={NoMatch} />
+    </Switch>
+  </div>
+)
 
 
 
